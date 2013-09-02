300b1ecfba74ee7fad3b5810d70ca6c6ad6a5750
Lucas <metallucas.ds@gmail.com>
2013-09-01 22:37:23 -0300

    :uk: add ops.html

--- js/app/main.js
+++ js/app/main.js
@@ -5,6 +5,19 @@
 
 define(["jquery", "pjax", "pjax-cookies"], function($) {
   $(function() {
-    alert('aaa');
-  })
+    if ($.support.pjax) {
+      var duration = 400;
+      $(document).pjax("a[data-pjax]", ".main-content", {
+        fragment: ".main-content",
+        duration: duration
+      }),
+      $(".main-content")
+        .bind("pjax:beforeSend", function() {
+          $(this).hide("slow")
+        })
+        .bind("pjax:complete", function() {
+          $(this).fadeIn(duration), console.log("Works!");
+        });
+    }
+  });
 });
